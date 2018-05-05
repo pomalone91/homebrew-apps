480e49c86ac77c178cdc49745bc6b65de00ade33
pomalone91 <pomalone91@gmail.com>
2018-05-05 09:48:12 -0600

    Update roll.rb

--- roll.rb
+++ roll.rb
@@ -1,6 +1,6 @@
 class Roll < Formula
   desc "diff tool with browser-based GUI"
-  homepage "https://github.com/pomalone91/roll"
+  homepage "https://github.com/pomalone91/termiroll"
   url "https://github.com/pomalone91/termiroll/releases/download/0.1.0/roll-0.1.0.tar.gz"
   sha256 "6680fea70618422efa145542ddabaa9b02cd432f4e50ec730b2b4700a2198954"
 
