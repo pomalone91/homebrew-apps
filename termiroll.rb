16c42b5a1317c304ed542d2b07574558b00bc7b7
pomalone91 <pomalone91@gmail.com>
2018-05-05 16:32:39 -0600

    Update termiroll.rb

--- termiroll.rb
+++ termiroll.rb
@@ -1,7 +1,7 @@
 class Termiroll < Formula
   desc "CLI based dice roller"
   homepage "https://github.com/pomalone91/termiroll"
-  url "https://github.com/pomalone91/roll/releases/download/0.1.0/roll-0.1.0.tar.gz"
+  url "https://github.com/pomalone91/termiroll/releases/download/1.0/roll-1.0.tar.gz"
   sha256 "6680fea70618422efa145542ddabaa9b02cd432f4e50ec730b2b4700a2198954"
 
   bottle :unneeded
