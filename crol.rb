0a1f8f36c0b17436c74df9d80cc4e65de5d34535
pomalone91 <pomalone91@gmail.com>
2020-04-04 15:30:21 -0600

    trying to fix sha issue again...

--- crol.rb
+++ crol.rb
@@ -2,7 +2,7 @@ class Crol < Formula
   desc "CLI based dice roller written in C"
   homepage "https://github.com/pomalone91/c_roll"
   url "https://github.com/pomalone91/c_roll/releases/download/v1.0/roll-1.0.tar.gz"
-  sha256 "7f4542d3e1dc14b26d85a6b3438358023a8bb5fcd4bf7cca4d261578045bfbab"
+  sha256 "722cb5769d4f6a79b82b79d28a7bc0fc472a54cd29d2f77f2c015e802cf8b66f"
 
   bottle :unneeded
 
@@ -14,3 +14,6 @@ class Crol < Formula
     system "#{bin}/roll"
   end
 end
+
+7f4542d3e1dc14b26d85a6b3438358023a8bb5fcd4bf7cca4d261578045bfbab
+722cb5769d4f6a79b82b79d28a7bc0fc472a54cd29d2f77f2c015e802cf8b66f
