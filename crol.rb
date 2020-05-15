efa33ba60f57c637a683617109218a02059f812f
pomalone91 <pomalone91@gmail.com>
2020-05-14 22:08:45 -0600

    Fixing version number in URL

--- crol.rb
+++ crol.rb
@@ -1,7 +1,7 @@
 class Crol < Formula
   desc "CLI based dice roller written in C"
   homepage "https://github.com/pomalone91/c_roll"
-  url "https://github.com/pomalone91/c_roll/releases/download/v1.0/roll-1.1.tar.gz"
+  url "https://github.com/pomalone91/c_roll/releases/download/v1.1/roll-1.1.tar.gz"
   sha256 "c6b13848db7efbbf2132249ad5777f8597528e14fbd11c7c79364afbe21e922d"
 
   bottle :unneeded
